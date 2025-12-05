20019f958fab16535f8aeda7b23a513dc4ef0626
HOperGT <emilshig5@gmail.com>
2025-12-05 15:57:18 +0300

    KS-1-5-fix-constant-spaces

--- src/main/kotlin/KS_1/KS_1_5.kt
+++ src/main/kotlin/KS_1/KS_1_5.kt
@@ -1,14 +1,16 @@
 package org.example.KS_1
 
+const val TIME_UNIT: Byte = 60
+
 fun main(){
     var second: Short
     var hour: Byte
     var minute: Byte
-    val TIME_UNIT: Byte = 60
+
 
     second = 6480
-    hour = (second / (TIME_UNIT*TIME_UNIT)).toByte()
-    minute = ((second % (TIME_UNIT*TIME_UNIT)) / TIME_UNIT).toByte()
+    hour = (second / (TIME_UNIT * TIME_UNIT)).toByte()
+    minute = ((second % (TIME_UNIT * TIME_UNIT)) / TIME_UNIT).toByte()
     second = (second % TIME_UNIT).toShort()
     print("%02d:%02d:%02d".format(hour, minute, second))
 
