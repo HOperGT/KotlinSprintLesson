98521ab4253cc1daa9410a344629fabb045d3dd9
HOperGT <emilshig5@gmail.com>
2025-12-06 17:16:27 +0300

    KS-2-4-fix-add-point-variables

--- src/main/kotlin/KS_2/KS_2_1.kt
+++ src/main/kotlin/KS_2/KS_2_1.kt
@@ -4,7 +4,11 @@ fun main(){
 
     var midPointer: Float = 0f
     val students: Byte = 4
-    val sumPoints: Short = (3 + 4 + 3 + 5).toShort()
+    val pointStud_1: Byte = 3
+    val pointStud_2: Byte = 4
+    val pointStud_3: Byte = 3
+    val pointStud_4: Byte = 5
+    val sumPoints: Short = (pointStud_1 + pointStud_2 + pointStud_3 + pointStud_4).toShort()
 
     midPointer = sumPoints.toFloat() / students
 
